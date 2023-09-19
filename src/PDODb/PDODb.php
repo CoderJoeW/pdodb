4e4b189f3ea40e249e594cf78baf6dd370037c26
Joseph Williamson <j.williamson@gambler-labs.com>
2023-09-19 05:14:29 +0100

    correct formatting issues

--- src/PDODb/PDODb.php
+++ src/PDODb/PDODb.php
@@ -6,7 +6,7 @@ use PDO;
 use PDOException;
 
 class Database{
-	private PDO $connection;
+    private PDO $connection;
 
     /**
      * @param string $username
@@ -15,51 +15,51 @@ class Database{
      * @param string $dbName
      * @param string $connectionType
     */
-	public function __construct($username = '', $password = '', $host = '', $dbName = '', $connectionType = 'mysql',array $attributes){
-		try{
-			$this->connection = new PDO("$connectionType:host=$host;port=3306;dbname=$dbName;charset=utf8mb4",$username,$password);
+    public function __construct($username = '', $password = '', $host = '', $dbName = '', $connectionType = 'mysql',array $attributes){
+        try{
+            $this->connection = new PDO("$connectionType:host=$host;port=3306;dbname=$dbName;charset=utf8mb4",$username,$password);
 
             foreach($attributes as $k => $v){
                 $this->connection->setAttribute($k, $v);
             }
-		}catch(PDOException $e){
-			echo $e->getMessage();
-		}
-	}
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
 
     /**
      * @param string $query
      * @param array<mixed> $params
     */
-	public function insert($query ,$params = []): string|bool{
-		$this->executeStatement($query,$params);
-		return $this->connection->lastInsertId();
-	}
+    public function insert($query ,$params = []): string|bool{
+        $this->executeStatement($query,$params);
+        return $this->connection->lastInsertId();
+    }
 
     /**
      * @param string $query
      * @param array<mixed> $params
     */
-	public function select($query, $params = []){
-		$statement = $this->executeStatement($query,$params);
-		return $statement->fetchAll();
-	}
+    public function select($query, $params = []){
+        $statement = $this->executeStatement($query,$params);
+        return $statement->fetchAll();
+    }
 
     /**
      * @param string $query
      * @param array<mixed> $params
     */
-	public function update($query, $params = []){
-		$this->executeStatement($query,$params);
-	}
+    public function update($query, $params = []){
+        $this->executeStatement($query,$params);
+    }
 
     /**
      * @param string $query
      * @param array<mixed> $params
     */
-	public function remove($query, $params = []){
-		$this->executeStatement($query,$params);
-	}
+    public function remove($query, $params = []){
+        $this->executeStatement($query,$params);
+    }
     
     /**
      * @param array<mixed> $data
@@ -90,17 +90,19 @@ class Database{
      * @param string $query
      * @param array<mixed> $params
     */
-	private function executeStatement($query,$params = []){
-		$statement = $this->connection->prepare($query);
+    private function executeStatement($query,$params = []){
+        $statement = $this->connection->prepare($query);
+
         try{
-        $statement->execute($params);
+            $statement->execute($params);
         } catch (PDOException $e){
             error_log($e);
             error_log(print_r($params));
             error_log($query);
             return;
         }
+        
         return $statement;
-	}
+    }
 }
 ?>
